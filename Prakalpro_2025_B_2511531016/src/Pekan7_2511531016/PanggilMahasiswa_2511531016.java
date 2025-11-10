d7b91375e7c029f5dce400b70dcb4d86eb53b0c0
kinayanovryamanda7-blip <kinayanovryamanda7@gmail.com>
2025-11-10 11:37:50 +0700

    Add files via upload

---
+++ Prakalpro_2025_B_2511531016/src/Pekan7_2511531016/PanggilMahasiswa_2511531016.java
@@ -0,0 +1,14 @@
+package Pekan7_2511531016;
+
+public class PanggilMahasiswa_2511531016 {
+
+	public static void main(String[] args) {
+		Mahasiswa_2511531016 a= new Mahasiswa_2511531016();
+		a.setNim(1016);
+		a.setNama("Kinaya");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+	}
+
+}
